0e9ee74736922ae86d44cc580965862d32f03208
David R. Pugh <drobert.pugh@gmail.com>
2015-02-18 12:00:44 +0000

    Refactored the model class to deal with boundary conditions.

--- models.py
+++ models.py
@@ -46,25 +46,39 @@ class SymbolicModel(object):
 
         :getter: Return the right-hand side of the system of equations.
         :setter: Set a new right-hand side of the system of equations.
-        :type: sympy.Matrix
+        :type: dict
 
         """
         return self._rhs
 
     @rhs.setter
-    def rhs(self, system):
+    def rhs(self, equations):
         """Set a new right-hand side of the system of equations."""
-        self._rhs = self._validate_rhs(system)
+        self._rhs = self._validate_rhs(equations)
         self._clear_cache
 
     @staticmethod
-    def _validate_equation(equation):
-        """Validates a symbolic equation."""
-        if not isinstance(equation, sym.Basic):
+    def _validate_expression(expression):
+        """Validates a symbolic expression."""
+        if not isinstance(expression, sym.Basic):
             mesg = "Attribute must be of type `sympy.Basic` not {}"
-            raise AttributeError(mesg.format(equation.__class__))
+            raise AttributeError(mesg.format(expression.__class__))
         else:
-            return equation
+            return expression
+
+    def _validate_rhs(self, rhs):
+        """Validate a the rhs attribute."""
+        if not isinstance(rhs, dict):
+            mesg = "Attribute `rhs` must be of type `dict` not {}"
+            raise AttributeError(mesg.format(rhs.__class__))
+        elif not (len(rhs) == len(self.dependent_vars)):
+            mesg = "Number of equations must equal number of dependent vars."
+            raise ValueError(mesg)
+        else:
+            exprs = {}
+            for var, expr in rhs.iteritems():
+                exprs[var] = self._validate_expression(expr)
+            return exprs
 
     @staticmethod
     def _validate_symbol(symbol):
@@ -79,17 +93,6 @@ class SymbolicModel(object):
     def _validate_symbols(cls, symbols):
         """Validate the dependent_vars attribute."""
         return [cls._validate_symbol(symbol) for symbol in symbols]
-
-    def _validate_rhs(self, rhs):
-        """Validate the rhs attribute."""
-        if not isinstance(rhs, dict):
-            mesg = "Attribute must be of type `dict` not {}"
-            raise AttributeError(mesg.format(rhs.__class__))
-        elif not (len(rhs) == len(self.dependent_vars)):
-            mesg = "Number of equations must equal number of dependent vars."
-            raise ValueError(mesg)
-        else:
-            return dict((var, self._validate_equation(eqn)) for var, eqn in rhs.iteritems())
 
 
 class DifferentialEquation(SymbolicModel):
@@ -126,6 +129,69 @@ class DifferentialEquation(SymbolicModel):
         self.__symbolic_jacobian = None
 
 
+class BoundaryValueProblem(DifferentialEquation):
+    """Class for representing two-point boundary value problems."""
+
+    def __init__(self, boundary_conditions, dependent_vars, independent_var, rhs):
+        """
+        Create an instance of a two-point boundary value problem (BVP).
+
+        """
+        super(BoundaryValueProblem, self).__init__(dependent_vars,
+                                                   independent_var,
+                                                   rhs)
+        self.boundary_conditions = boundary_conditions
+
+    @property
+    def boundary_conditions(self):
+        """
+        Boundary conditions for the problem.
+
+        :getter: Return the boundary conditions for the problem.
+        :setter: Set new boundary conditions for the problem.
+        :type: dict
+
+        """
+        return self._boundary_conditions
+
+    @boundary_conditions.setter
+    def boundary_conditions(self, conditions):
+        """Set new boundary conditions for the model."""
+        self._boundary_conditions = self._validate_boundary(conditions)
+
+    def _sufficient_boundary(self, conditions):
+        """Check that there are sufficient boundary conditions."""
+        number_conditions = 0
+        if conditions['lower'] is not None:
+            number_conditions += len(conditions['lower'])
+        if conditions['upper'] is not None:
+            number_conditions += len(conditions['upper'])
+        return number_conditions == len(self.dependent_vars)
+
+    def _validate_boundary(self, conditions):
+        """Validate a dictionary of lower and upper boundary conditions."""
+        if not isinstance(conditions, dict):
+            mesg = "Attribute `boundary_conditions` must have type `dict` not {}"
+            raise AttributeError(mesg.format(conditions.__class__))
+        elif not (set(conditions.keys()) < set(['lower', 'upper', None])):
+            mesg = "Keys for `boundary_conditions` dict must be {}, {}, or {}"
+            raise AttributeError(mesg.format('lower', 'upper', 'None'))
+        elif not self._sufficient_boundary(conditions):
+            mesg = "Number of conditions must equal number of dependent vars."
+            raise ValueError(mesg)
+        else:
+            bcs = {'lower': self._validate_boundary_exprs(conditions['lower']),
+                   'upper': self._validate_boundary_exprs(conditions['upper'])}
+            return bcs
+
+    def _validate_boundary_exprs(self, expressions):
+        """Check that lower/upper boundary_conditions are expressions."""
+        if expressions is None:
+            return None
+        else:
+            return [self._validate_expression(expr) for expr in expressions]
+
+
 if __name__ == '__main__':
 
     # define some variables
@@ -141,16 +207,27 @@ if __name__ == '__main__':
 
     # define the Solow model
     k_dot = s * y - (g + n + delta) * k
+    rhs = {k: k_dot}
 
-    solow = DifferentialEquation(dependent_vars=[k],
+    bcs = {'lower': [k - 5.0], 'upper': None}  # hopefully this will work!
+
+    solow = BoundaryValueProblem(dependent_vars=[k],
                                  independent_var=t,
-                                 rhs={k: k_dot})
+                                 rhs=rhs,
+                                 boundary_conditions=bcs)
 
     # define the Ramsey-Cass-Coopmans model
     mpk = sym.diff(y, k, 1)
     k_dot = y - c - (g + n) * k
-    c_dot = ((mpk - rho - theta * g) / theta) * c
+    c_dot = ((mpk - delta - rho - theta * g) / theta) * c
+    rhs = {k: k_dot, c: c_dot}
 
-    ramsey = DifferentialEquation(dependent_vars=[k, c],
+    kstar = ((1 - alpha)**((sigma - 1) / sigma) /
+             ((alpha / (delta + rho + theta * g))**(1 - sigma) - (alpha / (1 - alpha)))**((sigma - 1) / sigma))
+    cstar = y.subs({k: kstar}) - (g + n) * kstar
+    bcs = {'lower': [k - 5.0], 'upper': [c - cstar]}
+
+    ramsey = BoundaryValueProblem(dependent_vars=[k, c],
                                   independent_var=t,
-                                  rhs={k: k_dot, c: c_dot})
+                                  rhs=rhs,
+                                  boundary_conditions=bcs)
