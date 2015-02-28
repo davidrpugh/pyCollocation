dd3040646f60d8994833f55d69f28d130d27a1c1
David R. Pugh <drobert.pugh@gmail.com>
2015-02-28 21:43:19 +0000

    Created a base class for all Solution objects.

--- orthogonal_collocation.py
+++ orthogonal_collocation.py
@@ -1,8 +1,8 @@
 import numpy as np
-import pandas as pd
 from scipy import optimize
 
 import solvers
+import solutions
 
 
 class OrthogonalCollocation(solvers.Solver):
@@ -104,7 +104,7 @@ class OrthogonalCollocationSolver(OrthogonalCollocation):
         return solution
 
 
-class OrthogonalCollocationSolution(OrthogonalCollocation):
+class OrthogonalCollocationSolution(solutions.Solution, OrthogonalCollocation):
     """Represents solution obtained using an OrthogonalCollocation solver."""
 
     def __init__(self, degrees, domain, kind, model, params, result):
@@ -116,29 +116,6 @@ class OrthogonalCollocationSolution(OrthogonalCollocation):
         self._domain = domain
         self._kind = kind
         self._result = self._validate_result(result)
-
-    @property
-    def _interpolation_knots(self):
-        """Interpolation knots to use when computing the final solution."""
-        return np.linspace(self.domain[0], self.domain[1], self.number_knots)
-
-    @property
-    def _residuals(self):
-        """Return the residuals stored as a dict of NumPy arrays."""
-        tmp = {}
-        for var, function in self.residual_functions.iteritems():
-            tmp[var] = pd.Series(function(self._interpolation_knots),
-                                 index=self._interpolation_knots)
-        return tmp
-
-    @property
-    def _solution(self):
-        """Return the solution stored as a dict of NumPy arrays."""
-        tmp = {}
-        for var, function in self.functions.iteritems():
-            tmp[var] = pd.Series(function(self._interpolation_knots),
-                                 index=self._interpolation_knots)
-        return tmp
 
     @property
     def coefficients(self):
@@ -163,17 +140,6 @@ class OrthogonalCollocationSolution(OrthogonalCollocation):
         return self._degrees
 
     @property
-    def domain(self):
-        """
-        Domain to use when constructing the approximating polynomials.
-
-        :getter: Return the `domain` attribute.
-        :type: list
-
-        """
-        return self._domain
-
-    @property
     def derivatives(self):
         """
         Derivatives of the approximating polynomials.
@@ -183,6 +149,17 @@ class OrthogonalCollocationSolution(OrthogonalCollocation):
 
         """
         return self._construct_basis_derivs(self.coefficients, self.kind, self.domain)
+
+    @property
+    def domain(self):
+        """
+        Domain over which the approximated solution is valid.
+
+        :getter: Return the `domain` attribute.
+        :type: list
+
+        """
+        return self._domain
 
     @property
     def functions(self):
@@ -205,90 +182,3 @@ class OrthogonalCollocationSolution(OrthogonalCollocation):
 
         """
         return self._kind
-
-    @property
-    def number_knots(self):
-        """
-        Number of interpolation knots to use when computing the solution.
-
-        :getter: Return the number of interpolation knots.
-        :setter: Set a new number of interpolation knots.
-        :type: int
-
-        """
-        return self._number_knots
-
-    @number_knots.setter
-    def number_knots(self, value):
-        """Set a new number of interpolation knots."""
-        self._number_knots = self._validate_number_knots(value)
-
-    @property
-    def result(self):
-        """
-        An instance of the `optimize.optimize.OptimizeResult` class that stores
-        the raw output of the `OrthogonalCollocationSolver`.
-
-        :getter: Return the `result` attribute.
-        :type: `optimize.optimize.OptimizeResult`
-
-        """
-        return self._result
-
-    @property
-    def residual_functions(self):
-        return self._construct_residual_funcs(self.derivatives, self.functions)
-
-    @property
-    def residuals(self):
-        """
-        Solution residuals represented as a Pandas `DataFrame`.
-
-        :getter: Return the `DataFrame` representing the solution residuals.
-        :type: `pandas.DataFrame`
-
-        """
-        return pd.DataFrame.from_dict(self._residuals)
-
-    @property
-    def solution(self):
-        """
-        Solution to the model represented as a Pandas `DataFrame`.
-
-        :getter: Return the `DataFrame` representing the current solution.
-        :type: `pandas.DataFrame`
-
-        """
-        return pd.DataFrame.from_dict(self._solution)
-
-    @property
-    def success(self):
-        """
-        True, if a solution was found by the `OrthogonalCollocationSolver`;
-        otherwise, False.
-
-        :getter: Return the `success` attribute.
-        :type: `boolean`
-
-        """
-        return self.result.success
-
-    @staticmethod
-    def _validate_number_knots(number):
-        """Validates the `number_knots` attribute."""
-        if not isinstance(number, int):
-            mesg = ("The 'number_knots' attribute must have type " +
-                    "'int', not {}.")
-            raise AttributeError(mesg.format(number.__class__))
-        else:
-            return number
-
-    @staticmethod
-    def _validate_result(result):
-        """Validates the `result` attribute."""
-        if not isinstance(result, optimize.optimize.OptimizeResult):
-            mesg = ("The 'result' attribute must have type " +
-                    "'optimize.optimize.OptimizeResult', not {}.")
-            raise AttributeError(mesg.format(result.__class__))
-        else:
-            return result
