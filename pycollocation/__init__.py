2cbaca71aff655b9b7cb4a0baf525079f48832e5
David R. Pugh <drobert.pugh@gmail.com>
2015-03-26 14:36:57 +0000

    Major refactor prior to pypi.

--- pycollocation/__init__.py
+++ pycollocation/__init__.py
@@ -2,6 +2,16 @@
 Objects imported here will live in the `pycollocation` namespace
 
 """
+__all__ = ["BoundaryValueProblem", "OrthogonalPolynomialSolver", "Solution"]
+
+
 from . import models
-from . import solvers
+from . import orthogonal_polynomials
 from . import solutions
+
+from . models import BoundaryValueProblem
+from . orthogonal_polynomials import OrthogonalPolynomialSolver
+from . solutions import Solution
+
+# Add Version Attribute
+from .version import version as __version__
