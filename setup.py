8983521865d23606dbe917dd72a3d18017619c89
David R. Pugh <drobert.pugh@gmail.com>
2015-04-10 14:16:29 +0100

    Deleted extra blank line.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 
-
 # Meta information
 DESCRIPTION = ("Python package for solving initial value problems (IVP) " +
                "and two-point boundary value problems (2PBVP) using the " +
