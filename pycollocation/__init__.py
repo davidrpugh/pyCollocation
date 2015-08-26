f1da311bca16b2f16b608e38f2d55e386d1e1c1e
David R. Pugh <drobert.pugh@gmail.com>
2015-08-26 07:40:49 +0100

    Renamed orthogonal_polynomials module.

--- pycollocation/__init__.py
+++ pycollocation/__init__.py
@@ -5,11 +5,11 @@ Objects imported here will live in the `pycollocation` namespace
 __all__ = ["TwoPointBVP", "TwoPointBVPLike", "PolynomialSolver",
            "Visualizer"]
 
-from . import orthogonal_polynomials
+from . import polynomials
 from . import visualizers
 
 from . bvp import *
-from . orthogonal_polynomials import PolynomialSolver
+from . polynomials import PolynomialSolver
 from . visualizers import Visualizer
 
 # Add Version Attribute
