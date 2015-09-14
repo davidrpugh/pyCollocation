98d8b7f4d5661dd70a49e4e73b65e40145b04adc
David R. Pugh <drobert.pugh@gmail.com>
2015-09-14 14:57:31 +0100

    Updated docs and simplified API.

--- pycollocation/solvers.py
+++ pycollocation/solvers.py
@@ -8,22 +8,44 @@ class SolverBase(object):
 
     @staticmethod
     def _array_to_list(coefs_array, indices_or_sections, axis=0):
-        """Splits an array into equal sections."""
+        """Splits an array into sections."""
         return np.split(coefs_array, indices_or_sections, axis)
-
-    @staticmethod
-    def _evaluate_functions(functions, xs):
-        """Return a list of functions evaluated at specified points."""
-        return [f(xs) for f in functions]
 
     @classmethod
     def _approximate_soln(cls, basis_kwargs, coefs_list, domain):
+        """
+        Parameters
+        ----------
+        basis_kwargs : dict(str: )
+        coefs_list : list(numpy.ndarray)
+        domain : tuple
+
+        Returns
+        -------
+        basis_derivs : list(function)
+        basis_funcs : list(function)
+
+        """
         basis_derivs = cls._construct_basis_derivs(coefs_list, domain, **basis_kwargs)
         basis_funcs = cls._construct_basis_funcs(coefs_list, domain, **basis_kwargs)
         return basis_derivs, basis_funcs
 
     @classmethod
     def _assess_approximate_soln(cls, basis_derivs, basis_funcs, domain, nodes, problem):
+        """
+        Parameters
+        ----------
+        basis_derivs : list(function)
+        basis_funcs : list(function)
+        domain : tuple
+        nodes : numpy.ndarray
+        problem : TwoPointBVPLike
+
+        Returns
+        -------
+        resids : numpy.ndarray
+
+        """
         resid_func = cls._construct_residual_func(basis_derivs, basis_funcs, problem)
         interior_resids = resid_func(nodes)
         boundary_resids = cls._compute_boundary_resids(basis_funcs, domain, problem)
@@ -32,13 +54,13 @@ class SolverBase(object):
 
     @classmethod
     def _compute_bcs_lower(cls, basis_funcs, boundary, problem):
-        evaluated_basis_funcs = cls._evaluate_functions(basis_funcs, boundary)
-        return problem.bcs_lower(boundary, *evaluated_basis_funcs, **problem.params)
+        evald_basis_funcs = [func(boundary) for func in basis_funcs]
+        return problem.bcs_lower(boundary, *evald_basis_funcs, **problem.params)
 
     @classmethod
     def _compute_bcs_upper(cls, basis_funcs, boundary, problem):
-        evaluated_basis_funcs = cls._evaluate_functions(basis_funcs, boundary)
-        return problem.bcs_upper(boundary, *evaluated_basis_funcs, **problem.params)
+        evald_basis_funcs = [func(boundary) for func in basis_funcs]
+        return problem.bcs_upper(boundary, *evald_basis_funcs, **problem.params)
 
     @classmethod
     def _compute_boundary_resids(cls, basis_funcs, domain, problem):
@@ -68,51 +90,80 @@ class SolverBase(object):
 
     @classmethod
     def _compute_rhs(cls, basis_funcs, nodes, problem):
-        evaluated_basis_funcs = cls._evaluate_functions(basis_funcs, nodes)
-        evaluated_rhs = problem.rhs(nodes, *evaluated_basis_funcs, **problem.params)
+        """
+        Compute the value of the right-hand side (RHS) of the...
+
+        Parameters
+        ----------
+        basis_funcs : list(function)
+        nodes : numpy.ndarray
+        problem : TwoPointBVPLike
+
+        Returns
+        -------
+        evaluated_rhs : list(float)
+
+        """
+        evald_basis_funcs = [func(nodes) for func in basis_funcs]
+        evaluated_rhs = problem.rhs(nodes, *evald_basis_funcs, **problem.params)
         return evaluated_rhs
 
     @classmethod
-    def _construct_basis_derivs(cls, coefs, domain, **kwargs):
+    def _construct_basis_derivs(cls, coefs, **kwargs):
         """Return a list of basis functions given a list of coefficients."""
-        return [cls.basis_derivs_factory(coef, domain, **kwargs) for coef in coefs]
+        return [cls.basis_derivs_factory(coef, **kwargs) for coef in coefs]
 
     @classmethod
-    def _construct_basis_funcs(cls, coefs, domain, **kwargs):
+    def _construct_basis_funcs(cls, coefs, **kwargs):
         """Return a list of basis functions given a list of coefficients."""
-        return [cls.basis_funcs_factory(coef, domain, **kwargs) for coef in coefs]
+        return [cls.basis_funcs_factory(coef, **kwargs) for coef in coefs]
 
     @classmethod
     def _construct_residual_func(cls, basis_derivs, basis_funcs, problem):
 
         def residual_func(points):
-            evaluated_lhs = cls._evaluate_functions(basis_derivs, points)
+            evaluated_lhs = [deriv(points) for deriv in basis_derivs]
             evaluated_rhs = cls._compute_rhs(basis_funcs, points, problem)
             return [lhs - rhs for lhs, rhs in zip(evaluated_lhs, evaluated_rhs)]
 
         return residual_func
 
     @classmethod
-    def _construct_soln(cls, basis_kwargs, domain, nodes, problem, result):
+    def _construct_soln(cls, basis_kwargs, nodes, problem, result):
+        """
+        Construct a representation of the solution to the boundary value problem.
+
+        Parameters
+        ----------
+        basis_kwargs : dict(str : )
+        domain : tuple
+        nodes : numpy.ndarray
+        problem : TwoPointBVPLike
+        result : OptimizeResult
+
+        Returns
+        -------
+        solution : SolutionLike
+
+        """
         soln_coefs = cls._array_to_list(result.x, problem.number_odes)
-        soln_derivs = cls._construct_basis_derivs(soln_coefs, domain, **basis_kwargs)
-        soln_funcs = cls._construct_basis_funcs(soln_coefs, domain, **basis_kwargs)
+        soln_derivs = cls._construct_basis_derivs(soln_coefs, **basis_kwargs)
+        soln_funcs = cls._construct_basis_funcs(soln_coefs, **basis_kwargs)
         soln_residual_func = cls._construct_residual_func(soln_derivs, soln_funcs, problem)
-        solution = solutions.Solution(basis_kwargs, domain, soln_funcs, nodes,
-                                      problem, soln_residual_func, result)
+        solution = solutions.Solution(basis_kwargs, soln_funcs, nodes, problem,
+                                      soln_residual_func, result)
         return solution
 
     @classmethod
-    def basis_derivs_factory(cls, coef, domain, **kwargs):
+    def basis_derivs_factory(cls, coef, **kwargs):
         raise NotImplementedError
 
     @classmethod
-    def basis_funcs_factory(cls, coef, domain, **kwargs):
+    def basis_funcs_factory(cls, coef, **kwargs):
         raise NotImplementedError
 
     @classmethod
-    def solve(cls, basis_kwargs, coefs_array, domain, nodes, problem,
-              **solver_options):
+    def solve(cls, basis_kwargs, coefs_array, nodes, problem, **solver_options):
         """
         Solve a boundary value problem using orthogonal collocation.
 
@@ -123,27 +174,25 @@ class SolverBase(object):
         coefs_array : numpy.ndarray
             Array of coefficients for basis functions defining the initial
             condition.
-        domain : array_like
         nodes : numpy.ndarray
-        problem : bvp.BVPLike
-            Instance of a BVP problem to solve.
+        problem : bvp.TwoPointBVPLike
+            A two-point boundary value problem (BVP) to solve.
         solver_options : dict
             Dictionary of options to pass to the non-linear equation solver.
 
         Return
         ------
-        result : scipy.optimize.Result
-            Result object.
+        solution: solutions.SolutionLike
+            An instance of the SolutionLike class representing the solution to
+            the two-point boundary value problem (BVP)
 
         Notes
         -----
-        Collocation method converts functional equation into a system of
-        non-linear equations.
 
         """
         result = optimize.root(cls._compute_collocation_resids,
                                x0=coefs_array,
-                               args=(basis_kwargs, domain, nodes, problem),
+                               args=(basis_kwargs, nodes, problem),
                                **solver_options)
-        solution = cls._construct_soln(basis_kwargs, domain, nodes, problem, result)
+        solution = cls._construct_soln(basis_kwargs, nodes, problem, result)
         return solution
