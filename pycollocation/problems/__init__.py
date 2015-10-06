d82097ed27fa4ac0b331b2f6939d6e1faca8f926
David R. Pugh <drobert.pugh@gmail.com>
2015-10-06 07:20:34 +0100

    Minor changes to namespace.

--- pycollocation/problems/__init__.py
+++ pycollocation/problems/__init__.py
@@ -2,7 +2,7 @@
 Objects imported here will live in the `pycollocation.problems` namespace
 
 """
-from . bvp import TwoPointBVP
+from . bvp import TwoPointBVP, TwoPointBVPLike
 from . ivp import IVP
 
-__all__ = ["IVP", "TwoPointBVP"]
+__all__ = ["IVP", "TwoPointBVP", "TwoPointBVPLike"]
