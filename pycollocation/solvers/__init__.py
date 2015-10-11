cd856bd1959d4ed928e5ddbab62a766a962fd9a7
David R. Pugh <drobert.pugh@gmail.com>
2015-10-11 08:20:43 +0100

    Move LeastSquaresSolver' to its own module.

--- pycollocation/solvers/__init__.py
+++ pycollocation/solvers/__init__.py
@@ -2,7 +2,8 @@
 Objects imported here will live in the `pycollocation.solvers` namespace
 
 """
-from . solvers import LeastSquaresSolver, Solver, SolverLike
+from . over_identified import LeastSquaresSolver
+from . solvers import Solver, SolverLike
 from . solutions import SolutionLike, Solution
 
 __all__ = ["LeastSquaresSolver", "Solver", "Solution", "SolutionLike",
